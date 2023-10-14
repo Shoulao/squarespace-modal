6867c1c981b17f04f41467d34dce5880a4b1a8ac
Marcin Woźniak <marcinwozniak@MacBook-Pro-Marcin.local>
2023-10-14 18:58:41 +0200

    add openModal fn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
 function createModal(modalId, contentElement) {
-  const modal = document.createElement("div");
+  var modal = document.createElement("div");
   modal.id = modalId;
   modal.className = "modal";
 
-  const modalContent = document.createElement("div");
+  var modalContent = document.createElement("div");
   modalContent.className = "modal-content";
 
-  const closeBtn = document.createElement("span");
+  var closeBtn = document.createElement("span");
   closeBtn.className = "close";
   closeBtn.innerHTML = "&times;";
   closeBtn.onclick = function () {
     closeModal(modalId);
   };
 
-  // Append the passed content element to the modal content div
+  modalContent.appendChild(closeBtn);
   modalContent.appendChild(contentElement);
-  modalContent.appendChild(closeBtn); // Consider moving the close button to the top
-
   modal.appendChild(modalContent);
 
   document.body.appendChild(modal);
 }
 
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
 // Create a div with some child elements
-const customContent = document.createElement("div");
-const child1 = document.createElement("p");
+var customContent = document.createElement("div");
+var child1 = document.createElement("p");
 child1.textContent = "Child 1 content";
-const child2 = document.createElement("p");
+var child2 = document.createElement("p");
 child2.textContent = "Child 2 content";
 
 customContent.appendChild(child1);
