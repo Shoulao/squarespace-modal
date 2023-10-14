450863403b119e98b147096c1bc36fceff0d3d60
Marcin Woźniak <marcinwozniak@MacBook-Pro-Marcin.local>
2023-10-14 19:18:48 +0200

    update logic

--- dist/index.9730434a.js
+++
@@ -1,6 +0,0 @@
-// Create a div with some child elements
-const e=document.createElement("div"),t=document.createElement("p");t.textContent="Child 1 content";const n=document.createElement("p");n.textContent="Child 2 content",e.appendChild(t),e.appendChild(n),// Now create your modal
-function(e,t){let n=document.createElement("div");n.id=e,n.className="modal";let d=document.createElement("div");d.className="modal-content";let c=document.createElement("span");c.className="close",c.innerHTML="&times;",c.onclick=function(){closeModal(e)},// Append the passed content element to the modal content div
-d.appendChild(t),d.appendChild(c),n.appendChild(d),document.body.appendChild(n)}("modal1",e);//# sourceMappingURL=index.9730434a.js.map
-
-//# sourceMappingURL=index.9730434a.js.map
--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-function createModal(modalId, contentElement) {
-  var modal = document.createElement("div");
-  modal.id = modalId;
-  modal.className = "modal";
-
-  var modalContent = document.createElement("div");
-  modalContent.className = "modal-content";
-
-  var closeBtn = document.createElement("span");
-  closeBtn.className = "close";
-  closeBtn.innerHTML = "&times;";
-  closeBtn.onclick = function () {
-    closeModal(modalId);
+window.addEventListener("DOMContentLoaded", () => {
+  window.openModal = function (modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
   };
 
-  modalContent.appendChild(closeBtn);
-  modalContent.appendChild(contentElement);
-  modal.appendChild(modalContent);
+  window.closeModal = function (modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+  };
 
-  document.body.appendChild(modal);
-}
+  function createModal(modalId, contentElement) {
+    const modal = document.createElement("div");
+    modal.id = modalId;
+    modal.className = "modal";
 
-function openModal(modalId) {
-  var modal = document.getElementById(modalId);
-  modal.style.display = "block";
-}
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
 
-function closeModal(modalId) {
-  var modal = document.getElementById(modalId);
-  modal.style.display = "none";
-}
+    const closeBtn = document.createElement("span");
+    closeBtn.className = "close";
+    closeBtn.innerHTML = "&times;";
+    closeBtn.onclick = function () {
+      window.closeModal(modalId);
+    };
 
-// Create a div with some child elements
-var customContent = document.createElement("div");
-var child1 = document.createElement("p");
-child1.textContent = "Child 1 content";
-var child2 = document.createElement("p");
-child2.textContent = "Child 2 content";
+    modalContent.appendChild(closeBtn);
+    modalContent.appendChild(contentElement);
+    modal.appendChild(modalContent);
 
-customContent.appendChild(child1);
-customContent.appendChild(child2);
+    document.body.appendChild(modal);
+  }
 
-// Now create your modal
-createModal("modal1", customContent);
+  // Create a div with some child elements
+  const customContent = document.createElement("div");
+  const child1 = document.createElement("p");
+  child1.textContent = "Child 1 content";
+  const child2 = document.createElement("p");
+  child2.textContent = "Child 2 content";
+
+  customContent.appendChild(child1);
+  customContent.appendChild(child2);
+
+  // Now create your modal
+  createModal("modal1", customContent);
+});
